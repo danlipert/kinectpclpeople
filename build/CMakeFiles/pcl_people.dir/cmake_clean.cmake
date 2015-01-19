3316b211d6b3c71c34ca7819a5a1a838c0c9fa8d
Dan Lipert <dan@hyperlayer.io>
2015-01-19 11:19:42 -0800

    initial commit

---
+++ build/CMakeFiles/pcl_people.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcl_people.dir/pcl_people.cpp.o"
+  "pcl_people.pdb"
+  "pcl_people"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl_people.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
